329e2faf456e4afe3deb521205b16cc3264e3090
Evanteichman <81187144+Evanteichman@users.noreply.github.com>
2021-06-30 08:01:57 -0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -17,82 +17,132 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post(); ?>
-
-			<!-- Home ACF -->
-			<!-- Output the intro message -->
-			<p><?php the_field('intro_message');?></p>
-
-			<!-- Output the featured services, right now its a list, will probably need to change this -->
+		<section class="home-intro">
 			<?php
-			$featured_posts = get_field('featured_services');
-			if( $featured_posts ): ?>
-				<h2>Featured Services</h2>
-				<ul>
-				<?php foreach( $featured_posts as $post ): 
+			while ( have_posts() ) :
+				the_post(); ?>
 
-					// Setup this post for WP functions (variable must be named $post).
-					setup_postdata($post); ?>
-					<li>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				<!-- Home ACF -->
+				<!-- Output the intro message -->
+				<p><?php the_field('intro_message');?></p>
+		</section>
+			<!-- Output the featured services -->
+			<section class="featured-services">
+				<?php
+				$featured_posts = get_field('featured_services');
+				if( $featured_posts ): ?>
+					<h2>Featured Services</h2>
+					<div>
+					<?php foreach( $featured_posts as $post ): 
+
+						// Setup this post for WP functions (variable must be named $post).
+						setup_postdata($post); ?>
+							
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+								<!-- change size of photo here?? -->
+								<?php the_post_thumbnail(''); ?>
+							</a>
+							
 						
-					</li>
-				<?php endforeach; ?>
-				</ul>
+					<?php endforeach; ?>
+					</div>
+					<?php 
+					// Reset the global post object so that the rest of the page works correctly.
+					wp_reset_postdata(); ?>
+				<?php endif; ?>
+			</section>
+
+			<section class="featured-blog">
+				<!-- Output the featured blog posts -->
+				<?php
+				$featured_posts = get_field('featured_blog_posts');
+				if( $featured_posts ): ?>
+				<h2>Featured Blog Posts</h2>
+					<!-- <ul> -->
+					<div>
+					<?php foreach( $featured_posts as $post ): 
+
+						// Setup this post for WP functions (variable must be named $post).
+						setup_postdata($post); ?>
+						<!-- <li> -->
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+								<!-- change size of photo here?? -->
+								<?php the_post_thumbnail(''); ?>
+							</a>
+						<!-- </li> -->
+					<?php endforeach; ?>
+					<!-- </ul> -->
+					</div>
+					<?php 
+					// Reset the global post object so that the rest of the page works correctly.
+					wp_reset_postdata(); ?>
+				<?php endif; ?>
+			</section>
+
+
+			<section class="team-members">
+				<!-- Output the featured blog posts -->
+				<?php
+				$featured_posts = get_field('team_members');
+				if( $featured_posts ): ?>
+				<h2>Team Members</h2>
+					<!-- <ul> -->
+					<div>
+					<?php foreach( $featured_posts as $post ): 
+
+						// Setup this post for WP functions (variable must be named $post).
+						setup_postdata($post); ?>
+						<!-- <li> -->
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+								<!-- change size of photo here?? -->
+								<?php the_post_thumbnail(''); ?>
+							</a>
+						<!-- </li> -->
+					<?php endforeach; ?>
+					<!-- </ul> -->
+					</div>
+					<?php 
+					// Reset the global post object so that the rest of the page works correctly.
+					wp_reset_postdata(); ?>
+				<?php endif; ?>
+			</section>
+
+			<div class="button">
+				<!-- output the call to action link--shop -->
 				<?php 
-				// Reset the global post object so that the rest of the page works correctly.
-				wp_reset_postdata(); ?>
-			<?php endif; ?>
+				$link = get_field('call_to_action_shop');
+				if( $link ): 
+					$link_url = $link['url'];
+					$link_title = $link['title'];
+					$link_target = $link['target'] ? $link['target'] : '_self';
+					?>
+					<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<?php endif; ?>
+			</div>
 
 
-
-			<!-- Output the featured blog posts, right now its a list, will probably need to change this -->
-			<?php
-			$featured_posts = get_field('featured_blog_posts');
-			if( $featured_posts ): ?>
-			<h2>Featured Blog Posts</h2>
-				<ul>
-				<?php foreach( $featured_posts as $post ): 
-
-					// Setup this post for WP functions (variable must be named $post).
-					setup_postdata($post); ?>
-					<li>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</li>
-				<?php endforeach; ?>
-				</ul>
-				<?php 
-				// Reset the global post object so that the rest of the page works correctly.
-				wp_reset_postdata(); ?>
-			<?php endif; ?>
-
-			<!-- output the call to action link--shop -->
-			<?php 
-			$link = get_field('call_to_action_shop');
-			if( $link ): 
-				$link_url = $link['url'];
-				$link_title = $link['title'];
-				$link_target = $link['target'] ? $link['target'] : '_self';
-				?>
-				<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-			<?php endif; ?>
-
-
-
+			<div class="button">
 			<!-- output the call to action link--contact -->
-			<?php 
-			$link = get_field('call_to_action_contact');
-			if( $link ): 
-				$link_url = $link['url'];
-				$link_title = $link['title'];
-				$link_target = $link['target'] ? $link['target'] : '_self';
-				?>
-				<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-			<?php endif; ?>
+				<?php 
+				$link = get_field('call_to_action_contact');
+				if( $link ): 
+					$link_url = $link['url'];
+					$link_title = $link['title'];
+					$link_target = $link['target'] ? $link['target'] : '_self';
+					?>
+					<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				<?php endif; ?>
+			</div>
 
+			<?php
+			if ( get_field('instagram') ) {
+				echo do_shortcode( get_field('instagram') );
+			}		
 
+			?>
 
 
 			<!-- not sure if the code below is needed -->
