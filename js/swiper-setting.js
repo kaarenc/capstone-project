e37bf67b4004030fe12aed9c1d8bbc01f058c33d
Hunter <81187405+Hunter-Paulson@users.noreply.github.com>
2021-07-12 13:02:18 -0700

    Updates for home page

--- js/swiper-setting.js
+++ js/swiper-setting.js
@@ -10,11 +10,7 @@ const swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
     slidesPerView: 1,
-    spaceBetween: 10,
-    breakpoints: {
-        800: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
+    spaceBetween: 20,
+    
     }
-});
+);
